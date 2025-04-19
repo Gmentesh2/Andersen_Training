f44e8237c41c2c4c2ae6dda62e8033bd009be8ae
Gmentesh2 <g.menteshashvili.sg18@gipa.ge>
2025-04-19 13:47:22 +0400

    Display Pokemon details

--- src/components/pokemon-details/PokemonDetails.tsx
+++ src/components/pokemon-details/PokemonDetails.tsx
@@ -1,10 +1,41 @@
 
-const PokemonDetails = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+type PokemonDetailsProps = {
+  image: string;
+  name: string;
+  id: number;
+  height: number;
+  weight: number;
+  stats: {
+    name: string;
+    value: number;
+  }[];
+};
 
-export default PokemonDetails
+const PokemonDetails = ({
+  image,
+  name,
+  id,
+  height,
+  weight,
+  stats,
+}: PokemonDetailsProps) => {
+  return (
+    <div className={"pokemon-details-card"}>
+      <img src={image} alt={name} />
+      <h2>{name}</h2>
+      <p>ID: {id}</p>
+      <p>Height: {height}</p>
+      <p>Weight: {weight}</p>
+      <h3>Stats</h3>
+      <ul>
+        {stats.map((stat) => (
+          <li key={stat.name}>
+            {stat.name}: {stat.value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PokemonDetails;
--- src/components/pokemon-list/PokemonList.tsx
+++ src/components/pokemon-list/PokemonList.tsx
@@ -1,11 +1,62 @@
+import PokemonDetails from "../pokemon-details/PokemonDetails";
 import styles from "./pok-list.module.css";
 
 const pokemonList = [
-  { name: "Ditto", id: 132 },
-  { name: "Pikachu", id: 25 },
-  { name: "Charizard", id: 6 },
-  { name: "Bulbasaur", id: 1 },
-  { name: "Squirtle", id: 7 },
+  {
+    name: "Ditto",
+    id: 132,
+    height: 3,
+    weight: 40,
+    stats: [
+      { name: "hp", value: 48 },
+      { name: "attack", value: 48 },
+      { name: "defense", value: 48 },
+    ],
+  },
+  {
+    name: "Pikachu",
+    id: 25,
+    height: 4,
+    weight: 60,
+    stats: [
+      { name: "hp", value: 35 },
+      { name: "attack", value: 55 },
+      { name: "defense", value: 40 },
+    ],
+  },
+  {
+    name: "Charizard",
+    id: 6,
+    height: 17,
+    weight: 905,
+    stats: [
+      { name: "hp", value: 78 },
+      { name: "attack", value: 84 },
+      { name: "defense", value: 78 },
+    ],
+  },
+  {
+    name: "Bulbasaur",
+    id: 1,
+    height: 7,
+    weight: 69,
+    stats: [
+      { name: "hp", value: 45 },
+      { name: "attack", value: 49 },
+      { name: "defense", value: 49 },
+    ],
+  },
+  {
+    name: "Squirtle",
+    id: 7,
+    height: 5,
+    weight: 90,
+    stats: [
+      { name: "hp", value: 44 },
+      { name: "attack", value: 48 },
+      { name: "defense", value: 65 },
+    ],
+  },
 ];
 
 const PokemonList = () => {
@@ -16,19 +67,14 @@ const PokemonList = () => {
         <section className={styles.cards}>
           {pokemonList.map((pokemon) => (
             <div key={pokemon.id} className={styles.card}>
-              <img
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
-                alt={pokemon.name}
-                className={styles.image}
+              <PokemonDetails
+                image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                name={pokemon.name}
+                id={pokemon.id}
+                height={pokemon.height}
+                weight={pokemon.weight}
+                stats={pokemon.stats}
               />
-              <h3>
-                <span>Name:</span> {pokemon.name}
-              </h3>
-              <p>ID: {pokemon.id}</p>
-              <div className={styles.buttons}>
-                <button>Toggle favorites</button>
-                <button>Toggle comparison</button>
-              </div>
             </div>
           ))}
         </section>
