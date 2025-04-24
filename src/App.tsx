d10eb484577050de149472a9219b053b7666d687
Gmentesh2 <g.menteshashvili.sg18@gipa.ge>
2025-04-24 11:49:46 +0400

    Add favorite pokemon components with mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from "./components/header/Header"
 import "./App.css"
-import PokemonList from "./components/pokemon-list/PokemonList"
+// import PokemonList from "./components/pokemon-list/PokemonList"
+import FavoritePokemons from "./components/favorite-pokemons-list/FavoritePokemons"
 
 const App = () => {
   return (
     <>
       <Header /> 
-      <PokemonList />
+      {/* <PokemonList /> */}
+      <FavoritePokemons />
     </>
   )
 }
---
+++ src/components/favorite-pokemons-list/FavoritePokemons.tsx
@@ -0,0 +1,62 @@
+import styles from "./favorite-pokemons.module.css"
+const favoritePokemons = [
+  {
+    name: "Ditto",
+    id: 132,
+    height: 3,
+    weight: 40,
+    stats: [
+      { name: "hp", value: 48 },
+      { name: "attack", value: 48 },
+      { name: "defense", value: 48 },
+    ],
+  },
+  {
+    name: "Pikachu",
+    id: 25,
+    height: 4,
+    weight: 60,
+    stats: [
+      { name: "hp", value: 35 },
+      { name: "attack", value: 55 },
+      { name: "defense", value: 40 },
+    ],
+  },
+  {
+    name: "Charizard",
+    id: 6,
+    height: 17,
+    weight: 905,
+    stats: [
+      { name: "hp", value: 78 },
+      { name: "attack", value: 84 },
+      { name: "defense", value: 78 },
+    ],
+  },
+];
+
+const FavoritePokemons = () => {
+  return <div className={`container ${styles.container}`}>
+    <h1>Favorite Pokemons</h1>
+    <div className="pokemon-list">
+      {favoritePokemons.map((pokemon) => (
+        <div key={pokemon.id} className="pokemon-card">
+          <h2>{pokemon.name}</h2>
+          <p>ID: {pokemon.id}</p>
+          <p>Height: {pokemon.height}</p>
+          <p>Weight: {pokemon.weight}</p>
+          <h3>Stats:</h3>
+          <ul>
+            {pokemon.stats.map((stat) => (
+              <li key={stat.name}>
+                {stat.name}: {stat.value}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  </div>;
+};
+
+export default FavoritePokemons;
