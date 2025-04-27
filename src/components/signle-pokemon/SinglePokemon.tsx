51b0e1451a8acfed61e04eaf88a24bba3507aeb6
Gmentesh2 <g.menteshashvili.sg18@gipa.ge>
2025-04-27 15:51:58 +0400

    Add SinglePokemon component and path in favorites to details

--- src/components/signle-pokemon/SinglePokemon.tsx
+++ src/components/signle-pokemon/SinglePokemon.tsx
@@ -1,9 +1,89 @@
-
+import { useParams } from "react-router-dom";
+import PokemonDetails from "../pokemon-details/PokemonDetails";
 import styles from "./single-pokemon.module.css";
+
+const pokemonList = [
+  {
+    name: "Ditto",
+    id: 132,
+    height: 3,
+    weight: 40,
+    stats: [
+      { name: "hp", value: 48 },
+      { name: "attack", value: 48 },
+      { name: "defense", value: 48 },
+    ],
+  },
+  {
+    name: "Pikachu",
+    id: 25,
+    height: 4,
+    weight: 60,
+    stats: [
+      { name: "hp", value: 35 },
+      { name: "attack", value: 55 },
+      { name: "defense", value: 40 },
+    ],
+  },
+  {
+    name: "Charizard",
+    id: 6,
+    height: 17,
+    weight: 905,
+    stats: [
+      { name: "hp", value: 78 },
+      { name: "attack", value: 84 },
+      { name: "defense", value: 78 },
+    ],
+  },
+  {
+    name: "Bulbasaur",
+    id: 1,
+    height: 7,
+    weight: 69,
+    stats: [
+      { name: "hp", value: 45 },
+      { name: "attack", value: 49 },
+      { name: "defense", value: 49 },
+    ],
+  },
+  {
+    name: "Squirtle",
+    id: 7,
+    height: 5,
+    weight: 90,
+    stats: [
+      { name: "hp", value: 44 },
+      { name: "attack", value: 48 },
+      { name: "defense", value: 65 },
+    ],
+  },
+];
+
 const SinglePokemon = () => {
+  const { pokemonID } = useParams<{ pokemonID: string }>();
+  const pokemon = pokemonList.find((p) => p.id.toString() === pokemonID);
+
+  if (!pokemon) {
+    return (
+      <div className={`container ${styles.container}`}>
+        <h1>Pokemon not found</h1>
+      </div>
+    );
+  }
+
   return (
     <div className={`container ${styles.container}`}>
-      
+      <div key={pokemon.id} className={styles.singlePokemonCard}>
+        <PokemonDetails
+          image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+          name={pokemon.name}
+          id={pokemon.id}
+          height={pokemon.height}
+          weight={pokemon.weight}
+          stats={pokemon.stats}
+        />
+      </div>
     </div>
   );
 };
