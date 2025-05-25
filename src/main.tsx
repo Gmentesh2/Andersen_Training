46d1a3217bfddc772cf46e8637a9aa40926f2f69
Gmentesh2 <g.menteshashvili.sg18@gipa.ge>
2025-05-25 12:43:31 +0400

    Configure store for redux persist.

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./store/store.ts";
+import store, { persistor } from "./store/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor} >
+      <BrowserRouter>
+        <StrictMode>
+          <App />
+        </StrictMode>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import pokemonReducer from "./slices/PokemonSlice";
 import singlePokemonReducer from "./slices/SinglePokemonSlice";
 import favoritePokemonReducer from "./slices/FavoritePokemonSlice";
-import comparisonPokemonReducer from "./slices/ComparisonSlice"
+import comparisonPokemonReducer from "./slices/ComparisonSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
 
-const store = configureStore({
+// Combine reducers when using Redux persist.
+const rootReducer = combineReducers({
   reducer: {
     pokemonList: pokemonReducer,
     singlePokemon: singlePokemonReducer,
@@ -12,6 +16,20 @@ const store = configureStore({
     comparisonPokemons: comparisonPokemonReducer,
   },
 });
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["favoritePokemons"],
+};
+// Create a persisted reducer.
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
